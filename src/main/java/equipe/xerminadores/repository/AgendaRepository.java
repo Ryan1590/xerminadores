34b0f98075a634383d5cd135f101c206eafb1eb1
Marlon Apolonio <marlonapolonio@hotmail.com>
2025-05-30 17:11:14 -0300

    alterações na regra da tela de agendamentos

--- src/main/java/equipe/xerminadores/repository/AgendaRepository.java
+++ src/main/java/equipe/xerminadores/repository/AgendaRepository.java
@@ -3,5 +3,9 @@ package equipe.xerminadores.repository;
 import equipe.xerminadores.model.Agenda;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface AgendaRepository extends JpaRepository<Agenda, Long> {
+    List<Agenda> findByMedicoIdAndData(Long medicoId, LocalDate data);
 }
